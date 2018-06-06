322030cd1408fcd955442236a3768c545ac2ae59
Deyan Dobromirov <dvd_video@abv.bg>
2018-06-06 17:35:46 +0300

    Updated: Flash sensors Added: E2 integrated LQ-PID controller

---
+++ lua/entities/gmod_wire_expression2/core/custom/controller_loop.lua
@@ -0,0 +1,143 @@
+/******************************************************************************\
+  Expression 2 ability to process sttacets with LQ-PID controller
+\******************************************************************************/
+
+-- Register the type up here before the Extension Registration so that the Wire LQ-PID still works
+registerType("state_controller", "xrd", nil,
+	nil,
+	nil,
+	function(retval)
+		if retval == nil then return end
+		if !istable(retval) then error("Return value is neither nil nor a table, but a "..type(retval).."!",0) end
+	end,
+	function(v)
+		return !istable(v) or not v.HitPos
+	end
+)
+
+/******************************************************************************/
+
+E2Lib.RegisterExtension("state_controller", true, "Lets E2 to include mutiple controller state objects")
+
+--[[
+ * NewControl: Class hovering state processing manager. Implements a controller unit
+ * nTo   > Controller sampling time in seconds
+ * sName > Controller hash name differentiation
+]]--
+local mtControl = {}
+      mtControl.__index = mtControl
+      mtControl.__tostring = function(oCon)
+        return oCon:getType().."["..mtControl.__type.."] ["..tostring(oCon:getPeriod()).."]"..
+          "{T="..oCon:getTune()..",W="..oCon:getWindup()..",P="..oCon:getPower().."}"
+      end
+function NewControl(nTo, sName)
+  local mTo = (tonumber(nTo) or 0); if(mTo <= 0) then -- Sampling time [s]
+    return LogStatus("NewControl: Sampling time <"..tostring(nTo).."> invalid",nil) end
+  local self  = {}                 -- Place to store the methods
+  local mfAbs = math.abs           -- Function used for error absolute
+  local mfSgn = GetSign            -- Function used for error sign
+  local mErrO, mErrN  = 0, 0       -- Error state values
+  local mvCon, meInt  = 0, true    -- Control value and integral enabled
+  local mvP, mvI, mvD = 0, 0, 0    -- Term values
+  local mkP, mkI, mkD = 0, 0, 0    -- P, I and D term gains
+  local mpP, mpI, mpD = 1, 1, 1    -- Raise the error to power of that much
+  local mbCmb, mbInv, mbOn, mSatD, mSatU = true, false, true -- Saturation limits and settings
+  local mName, mType, mTune, mWind, mPow = tostring(sName or "N/A"), "", "", "", ""
+  setmetatable(self, mtControl)    -- Save the settings internally
+  function self:getValue(kV,eV,pV) return (kV*mfSgn(eV)*mfAbs(eV)^pV) end
+  function self:getGains() return mkP, mkI, mkD end
+  function self:setEnInt(bSt) meInt = tobool(bSt); return self end
+  function self:getEnInt() return meInt end
+  function self:getError() return mErrO, mErrN end
+  function self:getControl() return mvCon end
+  function self:getTune() return mTune end
+  function self:getWindup() return mWind end
+  function self:getPower() return mPow end
+  function self:getType() return mType end
+  function self:getPeriod() return mTo end
+  function self:setTune(sTune)
+    local symComp = GetOpVar("OPSYM_COMPONENT")
+    local sTune  = tostring(sTune or ""):Trim()
+    local arTune = symComp:Explode(sTune)
+    if(arTune[1] and (tonumber(arTune[1] or 0) > 0)) then
+      mkP = (tonumber(arTune[1] or 0)) -- Proportional term
+    else return LogStatus("NewControl.setTune: P-gain <"..tostring(arTune[1]).."> invalid",nil) end
+    if(arTune[2] and (tonumber(arTune[2] or 0) > 0)) then
+      mkI = (mTo / (2 * (tonumber(arTune[2] or 0)))) -- Discrete integral term approximation
+      if(mbCmb) then mkI = mkI * mkP end
+    else LogStatus("NewControl.setTune: I-gain <"..tostring(arTune[2]).."> skip") end
+    if(arTune[3] and (tonumber(arTune[3] or 0) > 0)) then
+      mkD = (tonumber(arTune[3] or 0) * mTo)  -- Discrete derivative term approximation
+      if(mbCmb) then mkD = mkD * mkP end
+    else LogStatus("NewControl.setTune: D-gain <"..tostring(arTune[3]).."> skip") end
+    mType = ((mkP > 0) and "P" or "")..((mkI > 0) and "I" or "")..((mkD > 0) and "D" or "")
+    -- Init multiple states using the table, so on duplication can be created easily
+    mTune = tostring(sTune or ""); return LogStatus("NewControl.setTune: <"..tostring(sTune or "")..">", self)
+  end
+  function self:setWindup(sWind)
+    local symComp = GetOpVar("OPSYM_COMPONENT")
+    local sWind = tostring(sWind or ""):Trim()
+    local arSat = symComp:Explode(sWind)
+    if(arSat and tonumber(arSat[1]) and tonumber(arSat[2])) then
+      arSat[1], arSat[2] = tonumber(arSat[1]), tonumber(arSat[2]) -- Saturation windup
+      if(arSat[1] < arSat[2]) then
+        mSatD, mSatU = arSat[1], arSat[2]
+        LogStatus("NewControl.setWindup: Bounds {"..tostring(mSatD).."<"..tostring(mSatU).."} load")
+      else LogStatus("NewControl.setWindup: Bounds {"..tostring(arSat[1]).."<"..tostring(arSat[2]).."} skip") end
+    else LogStatus("NewControl.setWindup: Windup <"..tostring(sWind or "").."> skip") end
+    mWind = tostring(sWind or ""); return LogStatus("NewControl.setWindup: OK", self)
+  end
+  function self:setPower(sPow)
+    local symComp = GetOpVar("OPSYM_COMPONENT")
+    local sPow  = tostring(sPow or ""):Trim()
+    local arPow = symComp:Explode(sPow) -- Power ignored when missing
+    if(arPow and tonumber(arPow[1]) and tonumber(arPow[2]) and tonumber(arPow[3])) then
+      mpP = (tonumber(arPow[1]) or 1) -- Proportional power
+      mpI = (tonumber(arPow[2]) or 1) -- Integral power
+      mpD = (tonumber(arPow[3]) or 1) -- Derivative power
+      LogStatus("NewControl.setPower: Power <"..tostring(mpP)..symComp..tostring(mpI)..symComp..tostring(mpD).."> load")
+    else LogStatus("NewControl.setPower: Power <"..tostring(sPow or "").."> skip") end
+    mPow = tostring(sPow or ""); return LogStatus("NewControl.setPower: OK", self)
+  end
+  function self:setFlags(bCmb, bInv)
+    mbCmb, mbInv = tobool(bCmb), tobool(bInv)
+    return LogStatus("NewControl.setFlags: OK", self) end
+  function self:Toggle(bOn) -- Executed in realtime
+    mbOn = tobool(bOn); return self end
+  function self:Reset() -- Executed in realtime
+    mErrO, mErrN, mvP, mvI, mvD, mvCon, meInt = 0, 0, 0, 0, 0, 0, true; return self end
+  function self:Process(vRef,vOut) -- Executed in realtime
+    if(not mbOn) then return self:Reset() end
+    mErrO = mErrN -- Refresh error state sample
+    mErrN = (mbInv and (vOut-vRef) or (vRef-vOut))
+    if(mkP > 0) then -- P-Term
+      mvP = self:getValue(mkP, mErrN, mpP) end
+    if((mkI > 0) and (mErrN ~= 0) and meInt) then -- I-Term
+      mvI = self:getValue(mkI, mErrN + mErrO, mpI) + mvI end
+    if((mkD > 0) and (mErrN ~= mErrO)) then -- D-Term
+      mvD = self:getValue(mkD, mErrN - mErrO, mpD) end
+    mvCon = mvP + mvI + mvD  -- Calculate the control signal
+    if(mSatD and mSatU) then -- Apply anti-windup effect
+      if    (mvCon < mSatD) then mvCon, meInt = mSatD, false
+      elseif(mvCon > mSatU) then mvCon, meInt = mSatU, false
+      else meInt = true end
+    end; return self
+  end
+  function self:Dump()
+    local sLabel = IfSelect(not IsEmptyString(mType), mType.."-", mType)
+    LogStatus("["..sLabel..mtControl.__type.."] Properties:")
+    LogStatus("  Name : "..mName.." ["..tostring(mTo).."]s")
+    LogStatus("  Param: {"..mTune.."}")
+    LogStatus("  Gains: {P="..tostring(mkP)..", I="..tostring(mkI)..", D="..tostring(mkD).."}")
+    LogStatus("  Power: {P="..tostring(mpP)..", I="..tostring(mpI)..", D="..tostring(mpD).."}\n")
+    LogStatus("  Limit: {D="..tostring(mSatD)..",U="..tostring(mSatU).."}")
+    LogStatus("  Error: {"..tostring(mErrO)..", "..tostring(mErrN).."}")
+    LogStatus("  Value: ["..tostring(mvCon).."] {P="..tostring(mvP)..", I="..tostring(mvI)..", D="..tostring(mvD).."}")
+    return self -- The dump method
+  end; return LogStatus("NewControl: Create ["..mName.."]", self) -- The control object
+end
+
+
+local function newStateController(self, string sName)
+  local data = {}
+end
--- lua/entities/gmod_wire_expression2/core/custom/flash_sensors.lua
+++ lua/entities/gmod_wire_expression2/core/custom/flash_sensors.lua
@@ -1,148 +1,230 @@
-local arrSensors = {}
+local arrSensors, curKey = {}
 
 local function makeSensor(sType, oEnt, sKey)
-  local Sensor = arrSensors
-  if(not Sensor[sType]) then Sensor[sType] = {} end Sensor = Sensor[sType]
-  if(not Sensor[oEnt] ) then Sensor[oEnt]  = {} end Sensor = Sensor[oEnt]
-  if(not Sensor[sKey] ) then Sensor[sKey]  = {} end Sensor = Sensor[sKey]
-  return Sensor
+  local oSensor, oKey = arrSensors, (sKey or curKey)
+  if(not oSensor[sType]) then oSensor[sType] = {} end oSensor = oSensor[sType]
+  if(not oSensor[oEnt] ) then oSensor[oEnt]  = {} end oSensor = oSensor[oEnt]
+  if(not oSensor[oKey] ) then oSensor[oKey]  = {} end oSensor = oSensor[oKey]
+  return oSensor
 end
 
 local function findSensor(sType, oEnt, sKey)
-  local Sensor = arrSensors
-  if(Sensor and sType) then Sensor = Sensor[sType] end
-  if(Sensor and oEnt ) then Sensor = Sensor[oEnt]  end
-  if(Sensor and sKey ) then Sensor = Sensor[sKey]  end
-  return Sensor
+  local oSensor, oKey = arrSensors, (sKey or curKey)
+  if(oSensor and sType) then oSensor = oSensor[sType] end
+  if(oSensor and oEnt ) then oSensor = oSensor[oEnt]  end
+  if(oSensor and oKey ) then oSensor = oSensor[oKey]  end
+  return oSensor
 end
+
+--[[ **************************** TRACER **************************** ]]
 
 __e2setcost(8)
 e2function void entity:delTracer(string sKey)
   if(not (this and this:IsValid())) then return end
-  if(sKey == "ALL") then arrSensors["TRACER"] = nil else
-    local Tracer = findSensor("TRACER", this, sKey)
-    if(Tracer) then arrSensors["TRACER"][this][sKey] = nil end
-  end
+  if(sKey == "*") then arrSensors["TRACER"] = nil else
+    local oTracer = findSensor("TRACER", this, sKey)
+    if(oTracer) then arrSensors["TRACER"][this][sKey] = nil end
+  end; return this
 end
 
 __e2setcost(20)
 e2function void entity:addTracer(string sKey, vector vPos, vector vDir, number nLen)
   if(not (this and this:IsValid())) then return end
-  local Tracer = makeSensor("TRACER", this, sKey)
-  Tracer.Len = nLen -- How long the range is
+  local oTracer = makeSensor("TRACER", this, sKey)
+  oTracer.Len = nLen -- How long the range is
   -- Local tracer position the trace starts from
-  Tracer.Pos = Vector(vPos[1],vPos[2],vPos[3])
+  oTracer.Pos = Vector(vPos[1],vPos[2],vPos[3])
   -- Local tracer direction to read the data of
-  Tracer.Dir = Vector(vDir[1],vDir[2],vDir[3])
-  Tracer.Dir:Normalize() -- Normalize the direction
-  Tracer.Dir:Mul(Tracer.Len) -- Multiply to add in real-time
+  oTracer.Dir = Vector(vDir[1],vDir[2],vDir[3])
+  oTracer.Dir:Normalize() -- Normalize the direction
+  oTracer.Dir:Mul(oTracer.Len) -- Multiply to add in real-time
   -- http://wiki.garrysmod.com/page/Structures/TraceResult
-  Tracer.TrO = {--[[Trace output parameters]]}
+  oTracer.TrO = {} -- Trace output parameters
   -- http://wiki.garrysmod.com/page/Structures/Trace
-  Tracer.TrI = {
+  oTracer.TrI = {
     start  = Vector(), -- The start position of the trace
     endpos = Vector(), -- The end   position of the trace
     filter = {this},   -- Which entities the trace must ignore
     -- http://wiki.garrysmod.com/page/Enums/CONTENTS
     mask   = bit.bor(CONTENTS_HITBOX, CONTENTS_SOLID,
                      CONTENTS_WINDOW, CONTENTS_MOVEABLE),
-    output = Tracer.TrO,  -- Give it output to save the data in
-    ignoreworld = false } -- Should the trace ignore world or not
+    output = oTracer.TrO,  -- Give it output to save the data in
+    ignoreworld = false }; return this -- Should the trace ignore world or not
 end
 
 __e2setcost(15)
-e2function void entity:smpTracer(string sKey)
+e2function entity entity:smpTracer(string sKey)
   if(not (this and this:IsValid())) then return end
-  local Tracer = findSensor("TRACER", this, sKey)
-  if(not Tracer) then return end
+  local oTracer = findSensor("TRACER", this, sKey)
+  if(not oTracer) then return end
   local entAng = this:GetAngles()
-  local trData = Tracer.TrI
-  trData.start:Set(Tracer.Pos)
+  local trData = oTracer.TrI
+  trData.start:Set(oTracer.Pos)
   trData.start:Rotate(entAng)
   trData.start:Add(this:GetPos())
-  trData.endpos:Set(Tracer.Dir)
+  trData.endpos:Set(oTracer.Dir)
   trData.endpos:Rotate(entAng)
   trData.endpos:Add(trData.start)
   -- http://wiki.garrysmod.com/page/util/TraceLine
-  util.TraceLine(trData)
+  util.TraceLine(trData); curKey = sKey; return this
 end
 
 __e2setcost(5)
 e2function number entity:getTracerHit(string sKey)
   if(not (this and this:IsValid())) then return 0 end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and (Tracer.TrO.Hit and 1 or 0) or 0)
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and (oTracer.TrO.Hit and 1 or 0) or 0)
+end
+
+__e2setcost(5)
+e2function number entity:getTracerHit()
+  if(not (this and this:IsValid())) then return 0 end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and (oTracer.TrO.Hit and 1 or 0) or 0)
 end
 
 __e2setcost(5)
 e2function number entity:getTracerHitWorld(string sKey)
   if(not (this and this:IsValid())) then return 0 end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and (Tracer.TrO.HitWorld and 1 or 0) or 0)
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and (oTracer.TrO.HitWorld and 1 or 0) or 0)
+end
+
+__e2setcost(5)
+e2function number entity:getTracerHitWorld()
+  if(not (this and this:IsValid())) then return 0 end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and (oTracer.TrO.HitWorld and 1 or 0) or 0)
 end
 
 __e2setcost(8)
 e2function vector entity:getTracerHitPosition(string sKey)
   if(not (this and this:IsValid())) then return {0,0,0} end
-  local Tracer = findSensor("TRACER", this, sKey)
-  local HitPos = Tracer and Tracer.TrO.HitPos
-  return (HitPos and {HitPos[1], HitPos[2], HitPos[3]} or {0,0,0})
+  local oTracer = findSensor("TRACER", this, sKey)
+  local hitPos = oTracer and oTracer.TrO.HitPos
+  return (hitPos and {hitPos[1], hitPos[2], hitPos[3]} or {0,0,0})
 end
-  
+
+__e2setcost(8)
+e2function vector entity:getTracerHitPosition()
+  if(not (this and this:IsValid())) then return {0,0,0} end
+  local oTracer = findSensor("TRACER", this)
+  local hitPos = oTracer and oTracer.TrO.HitPos
+  return (hitPos and {hitPos[1], hitPos[2], hitPos[3]} or {0,0,0})
+end
+
 __e2setcost(8)
 e2function vector entity:getTracerHitNormal(string sKey)
   if(not (this and this:IsValid())) then return {0,0,0} end
-  local Tracer = findSensor("TRACER", this, sKey)
-  local HitNor = Tracer and Tracer.TrO.HitNormal
-  return (HitNor and {HitNor[1], HitNor[2], HitNor[3]} or {0,0,0})
+  local oTracer = findSensor("TRACER", this, sKey)
+  local hitNor = oTracer and oTracer.TrO.HitNormal
+  return (hitNor and {hitNor[1], hitNor[2], hitNor[3]} or {0,0,0})
+end
+
+__e2setcost(8)
+e2function vector entity:getTracerHitNormal()
+  if(not (this and this:IsValid())) then return {0,0,0} end
+  local oTracer = findSensor("TRACER", this)
+  local hitNor = oTracer and oTracer.TrO.HitNormal
+  return (hitNor and {hitNor[1], hitNor[2], hitNor[3]} or {0,0,0})
 end
 
 __e2setcost(8)
 e2function string entity:getTracerHitTexture(string sKey)
   if(not (this and this:IsValid())) then return "" end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and Tracer.TrO.HitTexture or "")
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and oTracer.TrO.HitTexture or "")
+end
+
+__e2setcost(8)
+e2function string entity:getTracerHitTexture()
+  if(not (this and this:IsValid())) then return "" end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and oTracer.TrO.HitTexture or "")
 end
 
 __e2setcost(8)
 e2function vector entity:getTracerPosition(string sKey)
   if(not (this and this:IsValid())) then return {0,0,0} end
-  local Tracer = findSensor("TRACER", this, sKey)
-  local OrgPos = Tracer and Tracer.TrO.StartPos
-  return (OrgPos and {OrgPos[1], OrgPos[2], OrgPos[3]} or {0,0,0})
+  local oTracer = findSensor("TRACER", this, sKey)
+  local orgPos = oTracer and oTracer.TrO.StartPos
+  return (orgPos and {orgPos[1], orgPos[2], orgPos[3]} or {0,0,0})
+end
+
+__e2setcost(8)
+e2function vector entity:getTracerPosition()
+  if(not (this and this:IsValid())) then return {0,0,0} end
+  local oTracer = findSensor("TRACER", this)
+  local orgPos = oTracer and oTracer.TrO.StartPos
+  return (orgPos and {orgPos[1], orgPos[2], orgPos[3]} or {0,0,0})
 end
 
 __e2setcost(5)
 e2function number entity:getTracerDistance(string sKey)
   if(not (this and this:IsValid())) then return 0 end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and (Tracer.TrO.Fraction * Tracer.Len) or 0)
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and (oTracer.TrO.Fraction * oTracer.Len) or 0)
+end
+
+__e2setcost(5)
+e2function number entity:getTracerDistance()
+  if(not (this and this:IsValid())) then return 0 end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and (oTracer.TrO.Fraction * oTracer.Len) or 0)
 end
 
 __e2setcost(5)
 e2function number entity:getTracerStartSolid(string sKey)
   if(not (this and this:IsValid())) then return 0 end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and (Tracer.TrO.StartSolid and 1 or 0) or 0)
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and (oTracer.TrO.StartSolid and 1 or 0) or 0)
+end
+
+__e2setcost(5)
+e2function number entity:getTracerStartSolid()
+  if(not (this and this:IsValid())) then return 0 end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and (oTracer.TrO.StartSolid and 1 or 0) or 0)
 end
 
 __e2setcost(5)
 e2function number entity:getTracerAllSolid(string sKey)
   if(not (this and this:IsValid())) then return 0 end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and (Tracer.TrO.AllSolid and 1 or 0) or 0)
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and (oTracer.TrO.AllSolid and 1 or 0) or 0)
+end
+
+__e2setcost(5)
+e2function number entity:getTracerAllSolid()
+  if(not (this and this:IsValid())) then return 0 end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and (oTracer.TrO.AllSolid and 1 or 0) or 0)
 end
 
 __e2setcost(5)
 e2function number entity:getTracerLeftSolid(string sKey)
   if(not (this and this:IsValid())) then return 0 end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and (Tracer.TrO.FractionLeftSolid * Tracer.Len) or 0)
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and (oTracer.TrO.FractionLeftSolid * oTracer.Len) or 0)
+end
+
+__e2setcost(5)
+e2function number entity:getTracerLeftSolid()
+  if(not (this and this:IsValid())) then return 0 end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and (oTracer.TrO.FractionLeftSolid * oTracer.Len) or 0)
 end
 
 __e2setcost(5)
 e2function entity entity:getTracerEntity(string sKey)
   if(not (this and this:IsValid())) then return nil end
-  local Tracer = findSensor("TRACER", this, sKey)
-  return (Tracer and Tracer.TrO.Entity or nil)
+  local oTracer = findSensor("TRACER", this, sKey)
+  return (oTracer and oTracer.TrO.Entity or nil)
+end
+
+__e2setcost(5)
+e2function entity entity:getTracerEntity()
+  if(not (this and this:IsValid())) then return nil end
+  local oTracer = findSensor("TRACER", this)
+  return (oTracer and oTracer.TrO.Entity or nil)
 end
