a78154101a658cc9df3a0dba896f688e053679f0
Deyan Dobromirov <dvd_video@abv.bg>
2018-06-08 17:20:23 +0300

    Updated: FSensor API          1. Added filter function for the trace          2. Added API for every trace result column          3. Added max distance clamp          4. Class name and entity list filters          5. Renamed booleans to fsensor:is<colum_name>          6. The name of the fsensor method contains the column name          7. Renamed sampling method to fsensor:smpData()          8. Added invalid fsensor for comparison          9. Added API for controlling the trace data structure

--- lua/entities/gmod_wire_expression2/core/custom/fsensor.lua
+++ lua/entities/gmod_wire_expression2/core/custom/fsensor.lua
@@ -19,11 +19,11 @@ registerType("fsensor", "xfs", nil,
 
 E2Lib.RegisterExtension("fsensor", true, "Lets E2 chips trace ray attachments and check for hits.")
 
-local function newFSensor(oEnt, vPos, vDir, nLen)
-  if(not (oEnt and oEnt:IsValid())) then return nil end
-  local oFSensor = {}
-  oFSensor.Ent = oEnt -- Store the base entity
-  oFSensor.Len = nLen -- How long the range is
+local function newFSensor(vEnt, vPos, vDir, nLen)
+  if(not (vEnt and vEnt:IsValid())) then return nil end
+  local oFSensor = {}; oFSensor.Cls = {} -- Table for storing the hit classes
+  oFSensor.Len = math.Clamp(nLen,-50000,50000) -- How long the length is
+  oFSensor.Ign = {[vEnt]=true} -- Store the base entity for ignore
   -- Local tracer position the trace starts from
   oFSensor.Pos = Vector(vPos[1],vPos[2],vPos[3])
   -- Local tracer direction to read the data of
@@ -34,13 +34,17 @@ local function newFSensor(oEnt, vPos, vDir, nLen)
   oFSensor.TrO = {} -- Trace output parameters
   -- http://wiki.garrysmod.com/page/Structures/Trace
   oFSensor.TrI = {
+    mask   = MASK_SOLID,
     output = oFSensor.TrO,
     start  = Vector(), -- The start position of the trace
     endpos = Vector(), -- The end   position of the trace
-    filter = {oEnt},   -- Which entities the trace must ignore
-    -- http://wiki.garrysmod.com/page/Enums/CONTENTS
-    mask   = bit.bor(CONTENTS_HITBOX, CONTENTS_SOLID,
-                     CONTENTS_WINDOW, CONTENTS_MOVEABLE),
+    filter = function(oEnt)
+      if(not (oEnt and oEnt:IsValid())) then return end
+      if(oFSensor.Ign[oEnt] ~= nil) then return end
+      local tCls, eCls = oFSensor.Cls, oEnt:GetClass()
+      if((next(tCls) ~= nil) and (tCls[eCls] == nil)) then return end
+      return true -- Finally we register the trace hit enabled
+    end
     ignoreworld = false } -- Should the trace ignore world or not
   return oFSensor
 end
@@ -55,13 +59,78 @@ registerOperator("ass", "xfs", "xfs", function(self, args)
 	return rhs
 end)
 
+__e2setcost(1)
+e2function fsensor noFSensor()
+	return nil
+end
+
 __e2setcost(20)
 e2function fsensor entity:setFSensor(vector vP, vector vD, number nL)
   return newFSensor(this, vP, vD, nL)
 end
 
+__e2setcost(5)
+e2function fsensor fsensor:addIgnoreEntityHit(entity vE)
+  if(not this) then return nil end
+  if(not (vE and vE:IsValid())) then return nil end
+  this.Ign[vE] = true; return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:remIgnoreEntityHit(entity vE)
+  if(not this) then return nil end
+  if(not (vE and vE:IsValid())) then return nil end
+  this.Ign[vE] = nil; return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:addClassHit(string sC)
+  if(not this) then return nil end
+  this.Cls[sC] = true; return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:remClassHit(string sC)
+  if(not this) then return nil end
+  this.Cls[sC] = nil; return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:setIgnoreWorld(number nS)
+  if(not this) then return nil end
+  this.TrI.ignoreworld = (nS ~= 0); return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:setOrigin(vector vO)
+  if(not this) then return nil end
+  this.Pos[1], this.Pos[2], this.Pos[3] = vO[1], vO[2], vO[3]
+  return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:setDirection(vector vD)
+  if(not this) then return nil end
+  this.Dir[1], this.Dir[2], this.Dir[3] = vD[1], vD[2], vD[3]
+  this.Dir:Normalize(); this.Dir:Mul(this.Len)
+  return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:setLength(number nL)
+  if(not this) then return nil end
+  this.Len = nL; this.Dir:Normalize();
+  this.Dir:Mul(this.Len); return this
+end
+
+__e2setcost(5)
+e2function fsensor fsensor:setMask(number nM)
+  if(not this) then return nil end
+  this.TrI.mask = nM; return this
+end
+
 __e2setcost(15)
-e2function fsensor fsensor:smp()
+e2function fsensor fsensor:smpData()
   if(not this) then return nil end; local entLoc = this.Ent
   if(not (entLoc and entLoc:IsValid())) then return this end
   local entAng, trData = entLoc:GetAngles(), this.TrI
@@ -76,21 +145,63 @@ e2function fsensor fsensor:smp()
 end
 
 __e2setcost(5)
-e2function number fsensor:getHit()
+e2function number fsensor:isHitNoDraw()
+  if(not this) then return 0 end
+  local trV = this.TrO.HitNoDraw
+  return (trV and 1 or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:isHitNonWorld()
+  if(not this) then return 0 end
+  local trV = this.TrO.HitNonWorld
+  return (trV and 1 or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:isHit()
   if(not this) then return 0 end
   local trV = this.TrO.Hit
   return (trV and 1 or 0)
 end
 
 __e2setcost(5)
-e2function number fsensor:getHitWorld()
+e2function number fsensor:isHitSky()
+  if(not this) then return 0 end
+  local trV = this.TrO.HitSky
+  return (trV and 1 or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:isHitWorld()
   if(not this) then return 0 end
   local trV = this.TrO.HitWorld
   return (trV and 1 or 0)
 end
 
+__e2setcost(5)
+e2function number fsensor:getHitBox()
+  if(not this) then return 0 end
+  local trV = this.TrO.HitBox
+  return (trV and trV or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:getMatType()
+  if(not this) then return 0 end
+  local trV = this.TrO.MatType
+  return (trV and trV or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:getHitGroup()
+  if(not this) then return 0 end
+  local trV = this.TrO.HitGroup
+  return (trV and trV or 0)
+end
+
 __e2setcost(8)
-e2function vector fsensor:getHitPosition()
+e2function vector fsensor:getHitPos()
   if(not this) then return {0,0,0} end
   local trV = this.TrO.HitPos
   return (trV and {trV[1], trV[2], trV[3]} or {0,0,0})
@@ -104,42 +215,85 @@ e2function vector fsensor:getHitNormal()
 end
 
 __e2setcost(8)
+e2function vector fsensor:getNormal()
+  if(not this) then return {0,0,0} end
+  local trV = this.TrO.Normal
+  return (trV and {trV[1], trV[2], trV[3]} or {0,0,0})
+end
+
+__e2setcost(8)
 e2function string fsensor:getHitTexture()
   if(not this) then return "" end
-  local trV = this.TrO.StartPos
+  local trV = this.TrO.HitTexture
   return tostring(trV or "")
 end
 
 __e2setcost(8)
-e2function vector fsensor:getPosition()
+e2function vector fsensor:getStartPos()
   if(not this) then return {0,0,0} end
   local trV = this.TrO.StartPos
   return (trV and {trV[1], trV[2], trV[3]} or {0,0,0})
 end
 
 __e2setcost(5)
-e2function number fsensor:getDistance()
+e2function number fsensor:getSurfaceProps()
+  if(not this) then return 0 end
+  local trV = this.TrO.SurfaceProps
+  return (trV and trV or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:getSurfacePropsName()
+  if(not this) then return 0 end
+  local trV = this.TrO.SurfaceProps
+  return (trV and util.GetSurfacePropName(trV) or 0)
+end
+
+
+__e2setcost(5)
+e2function number fsensor:getPhysicsBone()
+  if(not this) then return 0 end
+  local trV = this.TrO.PhysicsBone
+  return (trV and trV or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:getFraction()
+  if(not this) then return 0 end
+  local trV = this.TrO.Fraction
+  return (trV and trV or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:getFractionDistance()
   if(not this) then return 0 end
   local trV = this.TrO.Fraction
   return (trV and (trV * this.Len) or 0)
 end
 
 __e2setcost(5)
-e2function number fsensor:getStartSolid()
+e2function number fsensor:isStartSolid()
   if(not this) then return 0 end
   local trV = this.TrO.StartSolid
   return (trV and 1 or 0)
 end
 
 __e2setcost(5)
-e2function number fsensor:getAllSolid()
+e2function number fsensor:isAllSolid()
   if(not this) then return 0 end
   local trV = this.TrO.AllSolid
   return (trV and 1 or 0)
 end
 
 __e2setcost(5)
-e2function number fsensor:getLeftSolid()
+e2function number fsensor:getFractionLeftSolid()
+  if(not this) then return 0 end
+  local trV = this.TrO.FractionLeftSolid
+  return (trV and trV or 0)
+end
+
+__e2setcost(5)
+e2function number fsensor:getFractionLeftSolidDistance()
   if(not this) then return 0 end
   local trV = this.TrO.FractionLeftSolid
   return (trV and (trV * this.Len) or 0)
